e387e146c7932d2aa5b72f4efaa6e007832ac68e
mariesham <53845672+mariesham@users.noreply.github.com>
2019-08-08 18:38:31 +0800

    Update Q4.py

--- L1/Q4.py
+++ L1/Q4.py
@@ -1,7 +1,9 @@
 from math import*
-year= input ("What is the year?\n")
-if (int(year)/4 - abs(int(year)/4) == 0) and (int(year)/400-abs(int(year)/400) == 0):
+year= input ("Enter a year.\n")
+if (int(year)%4 == 0) and (int(year)%100 > 0):
    print("Yes, it is a leap year.")
-elif (int(year)/100-abs(int(year)/100) == 0):
+elif(int(year)%400 == 0):
+    print("Yes, it is a leap year")
+else:
     print("No, it is not a leap year")
 
