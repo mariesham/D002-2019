a7815ac9c39f1961d7cd87c8f167c8ad3641d6e4
mariesham <53845672+mariesham@users.noreply.github.com>
2019-08-09 12:00:53 +0800

    Update Q1.py

--- L2/Q1.py
+++ L2/Q1.py
@@ -1,13 +1,19 @@
 n=int(input("Please input a number larger than 2\n"))
-y=2
-while y<n/2:
-    if n%y!=0:
-        y=y+1
-        break
+x=2
+y=0
+while x<n:
+    if n%x>0:
+       x=x+1
+       y=1
+       continue
     else:
         print("It is not a prime number.")
+        y=0
+        break
+if y==1:
+    print("It is a prime number")
         
-print("It is a prime number.")
+
     
 
     
