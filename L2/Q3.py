c27c6afae4e28c5b47d6ff454964072fad9968fd
mariesham <53845672+mariesham@users.noreply.github.com>
2019-08-11 20:00:00 +0800

    Q3
    I am having difficulties in completing this assignment, this is I have for now.

---
+++ L2/Q3.py
@@ -0,0 +1,15 @@
+for n in [-120,14,93,320,1,-999]:
+    y=0
+    x=0
+    if n>y:
+        x==1
+        print(n)
+    else:
+        x==0
+
+
+while x==1:
+    if n>next(n):
+        max=n
+        n=next(n)
+    print("The maximum value is %d."%max)
