e7a63a45901e305ae59ac297dade013607310db9
mariesham <53845672+mariesham@users.noreply.github.com>
2019-08-12 09:56:00 +0800

    Update Q3.py

--- L2/Q3.py
+++ L2/Q3.py
@@ -1,15 +1,7 @@
+max=0
 for n in [-120,14,93,320,1,-999]:
     y=0
-    x=0
-    if n>y:
-        x==1
-        print(n)
-    else:
-        x==0
-
-
-while x==1:
-    if n>next(n):
+    if n>max:
         max=n
-        n=next(n)
-    print("The maximum value is %d."%max)
+print(max)
+        
