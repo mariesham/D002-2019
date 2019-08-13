5bf94303167f7752cd46d165395f5348f2fd5d26
mariesham <53845672+mariesham@users.noreply.github.com>
2019-08-13 23:28:08 +0800

    Q6
    There are two Q6 ( in lecture slides and here) , so I combined them together to make one banana guessing game.

--- L2/Q6.py
+++ L2/Q6.py
@@ -1,23 +1,32 @@
-#L2 Q6: Banana Guessing game
-
-#Step 1: Import necessary modules
+#As there are two Q6 for L2 (one in Lecture slides and one as a skeleton code)and the instructions to make this program is a bit unclear, I have combined both assignments.
 import random
-#Step 2: Welcome Message
-print('''Welcome to the Banana Guessing Game
-Dave hid some bananas. Your task is to find out the number of bananas he hid.''')
-#Step 3: Choose a random number between 1-100
+count=0
 n = random.randint(1,100)
-print ("shhh, Dave hides %s bananas" % n)
-# define a flag for found/not found and counter on how many trials
-found = False
-count = 0
-#Step 4: Give three chances to the players 
+
+print('''Welcome to the Banana Guessing Game
+Dave hid some bananas. Your task is to find out the number of bananas he hid. You have 3 chances.''')
+while count <3:
+    y=int(input("What is your guess?\n"))
+    if y!= n:
+        if (n-y)<10 and (y-n)<10:
+            print("You are quite close. Try again!")
+            count=count+1
+        elif (y-n)>10:
+            print("Your guess is too high. Try again!")
+            count=count+1
+        elif (n-y)>10:
+            print("Your guess is too low. Try again!")
+            count=count+1
 
 
 
-#Step 5: Display a message
-if found == True:
+
+
+while count ==3 or n==y:
+    if n==y:
         print('You got the correct guess in %d trials' % count)
         print('Dave\'s banana are now all yours!')
-else:
-        print("You failed to find the number of bananas Dave hid! Try again next")
+        break
+    else:
+        print("You failed to find the number of bananas. Dave hid %d bananas. Try again next time!"%n)
+        break
