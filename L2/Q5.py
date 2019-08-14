4dae9cd2106b18d1c053ece5e3ab6cce855f2798
mariesham <53845672+mariesham@users.noreply.github.com>
2019-08-14 08:20:47 +0800

    Q5

--- L2/Q5.py
+++ L2/Q5.py
@@ -1,64 +1,135 @@
-# L2 Q5: play rock-paper-scissor - Beat the King
-# You need to win the king three times in a row in order to throw him away from his throne
-# Carefully think about where a loop should be place
-# Suggested Logic:
-#
-# Repeat the following until you really win
-#        Challenge the king three times, in each challenge
-#               pick a choice for the King and a choice for the player
-#               Repeat this if it is draw
-#                      pick a choice for the King and a choice for the player
-#               if fail the challenge, break from this loop
-#        
-
-
-
-
-# Import necessary modules
 import random
+from random import randint
 
-print('Welcome to the rock-paper-scissor game!\nYou are going to play against a minion!')
-
-# ascii art from https://www.asciiart.eu/people/body-parts/hand-gestures
-print("Please input your choice")
-print("""
-1.                 2.                           3.
+print('Welcome to the rock-paper-scissor game!\nYou are going to play against a minion!\nYou have 3 turns to beat the minion!\n')
+x=0
+while x<3:
+    p_choice=int(input("""Please input your choice\n1.                 2.                           3.
     _______                 _______                      _______
 ---'   ____)            ---'   ____)____             ---'   ____)____
       (_____)                     ______)                      ______) 
       (_____)                     _______)                  __________)
       (____)                     _______)                  (____)
----.__(___)             ---.__________)              ---.__(___)
+---.__(___)             ---.__________)              ---.__(___)\n"""))
+    status=0
+    y=0
+    m_choice=randint(1,3)
+        
+    if p_choice ==1 and m_choice==3:
+        status=1
+    elif p_choice ==2 and m_choice ==1:
+        status=1
+    elif p_choice ==3 and m_choice ==2:
+        status=1
+    elif m_choice ==1 and p_choice==3:
+        status=2
+    elif m_choice ==2 and p_choice ==1:
+        status=2
+    elif m_choice ==3 and p_choice ==2:
+        status=2
+    elif m_choice ==1 and p_choice==1:
+        status=3
+    elif m_choice ==2 and p_choice ==2:
+        status=3
+    elif m_choice ==3 and p_choice ==3:
+        status=3
+    elif p_choice >3 or p_choice <1:
+        status =4
 
-""") # 1 for rock; 2 for paper; 3 for scissor
 
-# step1: get player's choice, save it in variable p_choice
-
-
-# step2: generate a random choice for minion, save it in variable m_choice
-
-
-
-
-# status is used for the win/lose/draw of the game
-# status = 1 means player wins; status = 2 means minion wins; status = 3 means draw;
-# status = 4 means user gives invalid input, e.g. player inputs -1 or 4
-status = 0 # initialized as 0
-# step 3: given choices from player and minion, decide the game status
+    if m_choice == 1:
+        print("Minion chooses rock!")
+    elif m_choice == 2:
+        print("Minion chooses paper!")
+    elif m_choice == 3:
+        print("Minion chooses scissor!")
 
 
 
+    if status ==4:
+        print("Invalid input. Please input number 1,2 or 3 only.")
+        x=x-1
+        continue
+    elif status ==1:
+        print("Player wins!!")
+        x=x+1
+        y=y+1
+    elif status ==2:
+        print("Minion wins!!")
+        x=x+1
+    elif status == 3:
+        print("Draw! One extra turn.")
+        x=x-1
+
+
+
+while x==3:
+    if y==3:
+        print("Player has beaten the king!! Challenge completed!")
+        print("""──────────▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+────────█═════════════════█
+──────█═════════════════════█
+─────█═══▄▄▄▄▄▄▄═══▄▄▄▄▄▄▄═══█
+────█═══█████████═█████████═══█
+────█══██▀────▀█████▀────▀██══█
+───██████──█▀█──███──█▀█──██████
+───██████──▀▀▀──███──▀▀▀──██████
+────█══▀█▄────▄██─██▄────▄█▀══█
+────█════▀█████▀───▀█████▀════█
+────█═════════════════════════█
+────█══════════▄▀▀▀▄══════════█
+───▐▓▓▌═══════▀═════▀═══════▐▓▓▌
+───▐▐▓▓▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐▓▓▌▌
+───█══▐▓▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄▓▌══█
+──█══▌═▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌═▐══█
+──█══█═▐▓▓▓▓▓▓▄▄▄▄▄▄▄▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▐██▀██▌▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▓▀▀▀▀▀▓▓▓▓▓▓▓▌═█══█
+──█══█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█══█
+─▄█══█▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌█══█▄
+─█████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─█████
+─██████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─██████
+──▀█▀█──▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌───█▀█▀
+─────────▐▓▓▓▓▓▓▌▐▓▓▓▓▓▓▌
+──────────▐▓▓▓▓▌──▐▓▓▓▓▌
+─────────▄████▀────▀████▄
+─────────▀▀▀▀────────▀▀▀▀""")
+        break
+    else:
+        print("Player has failed to beat the king. Challenge failed!")
+        print("""──────────▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+────────█═════════════════█
+──────█═════════════════════█
+─────█═══▄▄▄▄▄▄▄═══▄▄▄▄▄▄▄═══█
+────█═══█████████═█████████═══█
+────█══██▀────▀█████▀────▀██══█
+───██████──█▀█──███──█▀█──██████
+───██████──▀▀▀──███──▀▀▀──██████
+────█══▀█▄────▄██─██▄────▄█▀══█
+────█════▀█████▀───▀█████▀════█
+────█═════════════════════════█
+────█═════════▀▄═══▄▀═════════█
+───▐▓▓▌═════════▀▀▀═════════▐▓▓▌
+───▐▐▓▓▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐▓▓▌▌
+───█══▐▓▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄▓▌══█
+──█══▌═▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌═▐══█
+──█══█═▐▓▓▓▓▓▓▄▄▄▄▄▄▄▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▐██▀██▌▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▓▀▀▀▀▀▓▓▓▓▓▓▓▌═█══█
+──█══█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█══█
+─▄█══█▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌█══█▄
+─█████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─█████
+─██████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─██████
+──▀█▀█──▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌───█▀█▀
+─────────▐▓▓▓▓▓▓▌▐▓▓▓▓▓▓▌
+──────────▐▓▓▓▓▌──▐▓▓▓▓▌
+─────────▄████▀────▀████▄
+─────────▀▀▀▀────────▀▀▀▀""")
+        break
 
 
 
 
 
 
-# step4: display the minion's choice
-if m_choice == 1:
-    print("Minion chooses rock!")
-elif m_choice == 2:
-    print("Minion chooses paper!")
-elif m_choice == 3:
-    print("Minion chooses scissor!")
 
