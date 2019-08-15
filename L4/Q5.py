783a0438c76c692632e419dfedc44c4fbbdd0166
mariesham <53845672+mariesham@users.noreply.github.com>
2019-08-15 09:49:05 +0800

    Update Q5.py

--- L4/Q5.py
+++ L4/Q5.py
@@ -1,10 +1,8 @@
-secret=input("Enter a word.\n")
-opened=int(input("Enter the indexes of the letters to be revealed.\n"))
 def printer(secret, opened):
     i=0
     x=[]
     while i < len(secret):
-        if i == opened:
+        if i in opened:
             a=secret[i]
             x.append(a)
         else:
@@ -12,5 +10,4 @@ def printer(secret, opened):
         i= i + 1
     print(x)
 
-printer(secret, opened)
-# Note: you might use print(x, end="") to print x without changing line
+printer("avocado",[0,3])
